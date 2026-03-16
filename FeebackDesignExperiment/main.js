9ca7736080aa8a0ed3a67dd7573a8fd3fd757139
ltran1153 <s3963174@rmit.edu.vn>
2026-03-16 15:37:37 +1100

    update

---
+++ FeebackDesignExperiment/main.js
@@ -0,0 +1,36 @@
+// find elements to use
+
+const IntroDialog = document.getElementById("Warning")
+const Acknowledged = document.getElementById("Acknowledged")
+const PlayButton = document.getElementById("ClickButton")
+
+IntroDialog.showModal()
+
+Acknowledged.addEventListener("click", CloseDialog)
+
+function CloseDialog() {
+  IntroDialog.close()
+  Tone.start()
+}
+
+//tone synth init
+const synth = new Tone.Synth().toDestination()
+
+// play sound with tone
+
+function PlayNote() {
+  synth.triggerAttackRelease("C4", "8n")
+}
+
+PlayButton.addEventListener("click", PlayNote)
+
+function StartNote() {
+  synth.triggerAttack("C4")
+}
+
+function EndNote() {
+  synth.triggerRelease()
+}
+
+PlayButton.addEventListener("mousedown", StartNote)
+PlayButton.addEventListener("mouseup", EndNote)

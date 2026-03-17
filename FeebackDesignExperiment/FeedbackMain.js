7a359647753096be0ebeaaaae1095c746e2882be
ltran1153 <s3963174@rmit.edu.vn>
2026-03-17 23:46:45 +1100

    update

--- FeebackDesignExperiment/FeedbackMain.js
+++ FeebackDesignExperiment/FeedbackMain.js
@@ -3,6 +3,7 @@
 const IntroDialog = document.getElementById("Warning")
 const Acknowledged = document.getElementById("Acknowledged")
 const PlayButton = document.getElementById("ClickButton")
+const Number = document.getElementById("number")
 
 IntroDialog.showModal()
 
@@ -26,13 +27,39 @@ PlayButton.addEventListener("click", PlayNote)
 
 function StartNote() {
   synth.triggerAttack("C4")
-  document.body.style.backgroundColor = "black";
 }
 
 function EndNote() {
-  synth.triggerRelease()
-  document.body.style.backgroundColor = "yellow";
+  synth.triggerRelease("8n")
 }
 
 PlayButton.addEventListener("mousedown", StartNote)
 PlayButton.addEventListener("mouseup", EndNote)
+
+let count = 0
+
+PlayButton.addEventListener("click", () => {
+  count++
+  Number.textContent = count
+
+  if (count < 25) {
+    document.body.style.backgroundColor = "white"
+  } else if (count < 50) {
+    document.body.style.backgroundColor = "blue"
+  } else if (count < 75) {
+    document.body.style.backgroundColor = "red"
+  } else if (count < 100) {
+    document.body.style.backgroundColor = "yellow"
+  } else {
+    document.body.style.backgroundColor = "black"
+    PlayButton.removeEventListener("mousedown", StartNote)
+    PlayButton.removeEventListener("mouseup", EndNote)
+    PlayButton.removeEventListener("click", PlayNote)
+    PlayButton.addEventListener("mouseover", () => {
+      PlayButton.style.cursor = "not-allowed"
+    })
+    PlayButton.addEventListener("click", () => {
+      PlayButton.style.cursor = "not-allowed"
+    })
+  }
+})
